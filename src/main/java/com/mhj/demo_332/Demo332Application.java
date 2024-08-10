237b3c4d5f67f339cdbe3ee244501ea4a83b9f88
mhjmhj2002 <mhjmhj2002@gmail.com>
2024-08-10 16:24:10 -0300

    first commit

---
+++ src/main/java/com/mhj/demo_332/Demo332Application.java
@@ -0,0 +1,13 @@
+package com.mhj.demo_332;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo332Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo332Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhj/demo_332/Demo332ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhj.demo_332;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo332ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
